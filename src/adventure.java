f59f4ce38a96005241f04cf46e7b8fccad75375f
gerings17 <skyswims@yahoo.com>
2016-12-13 18:06:44 -0800

    game

--- src/adventure.java
+++ src/adventure.java
@@ -11,12 +11,32 @@ public class adventure {
 
 			String line = scanner.nextLine();
 			System.out.println(line);
+			if (line.equals("Go North")) {
+				y += 1;
+			}
+			if (line.equals("Go South")) {
+				y -= 1;
+			}
+			if (line.equals("Go East")) {
+				x += 1;
+			}
+			if (line.equals("Go West")) {
+				x -= 1;
+			}
 			if (line.equals("Look down")) {
 				seenNote = true;
 				System.out.println("You see a note");
 			}
+			if (x == 0 && y == 1) {
+				System.out.println("You see a strange cave will go there?");
+			}
+			else if (x == 0 && y == 2) {
+				System.out.println("You see a strange chest. Will you attempt to open it?");
+			}
+			System.out.println("Your coordinates are " + x + " " + y);
 			if (line.equals("Read note") && seenNote) {
-				System.out.println("Note...");
+				System.out.println("Say Go North or other directions to move. Enjoy your adventure!");
+
 			}
 		}
 	}
