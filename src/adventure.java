29850020169cbe2a225c97c3b9484da01c4f97db
rylr <league@WTS9.attlocal.net>
2016-12-20 18:22:18 -0800

    stuff

--- src/adventure.java
+++ src/adventure.java
@@ -7,10 +7,11 @@ public class adventure {
 		int y = 0;
 		int health = 100;
 		int bosshealth = 100;
-
+		int XP = 0;
 		boolean foundBoss = false;
 		boolean seenNote = false;
 		boolean foundKey = false;
+		boolean foundChest = false;
 		int boss = new Random().nextInt(4);
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("You look around what should you do?");
@@ -34,12 +35,24 @@ public class adventure {
 				seenNote = true;
 				System.out.println("You see a note");
 			}
+			if (x == 2 && y == 0) {
+				System.out.println("You see an old man who offers to sell you bonus health. Will you buy some with XP");
+				line = scanner.nextLine();
+				if (line.equals("Yes") && XP >= 50) {
+					System.out.println("Here you go");
+					health = health + 25;
+				} else if (XP < 25) {
+					System.out.println("Come back when you have more XP. P.S. you can get XP by killing monsters");
+				}
+			}
+
 			if (x == 0 && y == 1) {
+				foundChest = true;
 				System.out.println("You see a strange cave will go there?");
 			} else if (x == 0 && y == 2) {
 				System.out.println("You see a strange chest. Will you attempt to open it?");
 			}
-			if (line.equals("Yes") && !foundKey) {
+			if (line.equals("Yes") && !foundKey && foundChest) {
 				System.out.println("You need a key");
 			}
 			if (x == 2 && y == 4) {
@@ -63,10 +76,12 @@ public class adventure {
 			if (bosshealth == 0) {
 				foundKey = true;
 				System.out.println("Congrats! Go collect your prize with the key");
+				XP = XP + 1000;
 			}
 			if (line.equals("Yes") && foundKey) {
 				System.out.println("Congrats you found the totem and have beaten this game");
 			}
+			System.out.println("Your current XP is " + XP);
 			System.out.println("Your current health is " + health);
 			System.out.println("Your coordinates are " + x + " " + y);
 			if (line.equals("Read note") && seenNote) {
